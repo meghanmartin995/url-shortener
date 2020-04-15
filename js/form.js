ce93f938b1449cc80be83f5e4a04a5d5de90ad3b
Meghan Martin <meghanxmartin@gmail.com>
2020-04-15 15:00:58 +0200

    creates copy functionality

--- js/form.js
+++ js/form.js
@@ -1,22 +1,26 @@
 const form = document.querySelector('#url-form')
 const results = document.querySelector('.results')
+const button = document.querySelector('.button')
+
 
 const shortenUrl = (link) => {
   fetch(`https://rel.ink/api/links/?url=${link}/`, {
+
     method: "POST",
     headers: { "Content-type": "application/json" },
     body: JSON.stringify({ url:link })
+
    })
     .then(response => response.json())
     .then((data) => {
-    // console.log(data);
       const result = `
-      <div class="result">
-        <p>${link}</p>
-        <a href="https://rel.ink/${data.hashid}">https://rel.ink/${data.hashid}</a>
-        <button class="square-btn sign-up" type="button">Copy</button>
-      </div>`;
-        results.insertAdjacentHTML("beforeend", result);
+        <div class="result">
+          <p>${link}</p>
+          <a id="link" href="https://rel.ink/${data.hashid}">https://rel.ink/${data.hashid}</a>
+        </div>`;
+      results.insertAdjacentHTML("afterbegin", result);
+      const copyButton = `<button class="square-btn sign-up" type="button">Copy</button>`
+      button.insertAdjacentHTML("beforeend", copyButton);
     });
 };
 
@@ -26,6 +30,26 @@ form.addEventListener("submit", (event) => {
   shortenUrl(input.value);
 });
 
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+
+const addListener = () => {
+  button.addEventListener("click", (event) => {
+    const link = document.querySelector('#link').innerText;
+    copyToClipboard(link);
+    event.currentTarget.innerHTML = `<button class="square-btn purple-btn sign-up" type="button">Copied!</button>`;
+  });
+};
 
 
+addListener()
 
